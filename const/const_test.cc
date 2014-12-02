7742412cdd7964c5dcd319bbfe89088c09905f73
Dmitry Sigaev <dima.sigaev@gmail.com>
2014-12-02 21:27:55 +0300

    Run-Time Check Failure #3 - The variable 'i' is being used without being initialized. 	printf("i: %d \n", i);      // print: i

--- const/const_test.cc
+++ const/const_test.cc
@@ -4,7 +4,10 @@
 void main(void)
 {
   const int ci5 = 5;	/* constant int */
-  const int ci6;
+ /* const int ci6; ERROR: 'ci6' : const object must be initialized if not extern */	
+  int i;			      	// int
+  
 
-  printf("ci5: %d \n", ci5); // print: ci5 
+  printf("ci5: %d \n", ci5);  // print: ci5 
+	printf("i: %d \n", i);      // print: i 
 }
