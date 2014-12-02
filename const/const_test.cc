9943829d19f78a7d3509c0e71d8d186b233615d2
Dmitry Sigaev <dima.sigaev@gmail.com>
2014-12-02 21:46:01 +0300

    fix: Edit->Advanced->View White Space

--- const/const_test.cc
+++ const/const_test.cc
@@ -3,15 +3,15 @@
 
 void main(void)
 {
-  const int ci5 = 5;	/* constant int */
+  const int ci5 = 5;    /* constant int */
  /* const int ci6; ERROR: 'ci6' : const object must be initialized if not extern */	
-  int i;			      	// int
-	int *p_i;			      /* Pointer to int */
+  int i;                // int
+  int *p_i;             /* Pointer to int */
 
-  int const *p_ci;    /* Pointer to constant int */
-	int const (*p_ci2); /* Pointer to constant int */
-	const int *p_ci3;	  /* Pointer to constant int */
-	const int (*p_ci4);	/* Pointer to constant int */
+  int const *p_ci;      /* Pointer to constant int */
+  int const (*p_ci2);   /* Pointer to constant int */
+  const int *p_ci3;     /* Pointer to constant int */
+  const int (*p_ci4);   /* Pointer to constant int */
 
   
   printf("ci5: %d \n", ci5);  // print: ci5
