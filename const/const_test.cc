124feaa57585b4390df969de1098f8cb600e2305
Dmitry Sigaev <dima.sigaev@gmail.com>
2014-12-03 15:51:15 +0300

    Release Mode: Output: *p_ci: 1515680(0x1720a0) *p_ci2: 1515680(0x1720a0) ci5: 5(0x5) 1>  const_test.cc warning C4101: 'p_ci3' : unreferenced local variable warning C4101: 'i' : unreferenced local variable warning C4101: 'p_ci4' : unreferenced local variable  Generating code const_test.cc(19): warning C4700: uninitialized local variable 'p_ci' used const_test.cc(24): warning C4700: uninitialized local variable 'p_i' used const_test.cc(27): warning C4700: uninitialized local variable 'p_ci2' used  Finished generating code

--- const/const_test.cc
+++ const/const_test.cc
@@ -6,22 +6,27 @@ void main(void)
   const int ci5 = 5;    /* constant int */
  /* const int ci6; ERROR: 'ci6' : const object must be initialized if not extern */	
   int i;                // int
-  int *p_i = &i;             /* Pointer to int */
+  // int *p_i = &i;             /* Pointer to int */
+  int *p_i;             /* Pointer to int */
+
 
   int const *p_ci;      /* Pointer to constant int */
   int const (*p_ci2);   /* Pointer to constant int */
   const int *p_ci3;     /* Pointer to constant int */
   const int (*p_ci4);   /* Pointer to constant int */
 
-  p_ci = &i;                      /* Good case */ /* currently uninitialized int */
-  printf("*p_ci: %d \n", *p_ci);  /* print: currently uninitialized int */
+//  p_ci = &i;                      /* Good case */ /* currently uninitialized int */
+  printf("*p_ci: %d(0x%x) \n", *p_ci, *p_ci);  /* print: currently uninitialized int 
+                                     compiled case but it has run-time error
+                                     in Debug Mode(VS2010) and ok in Release 
+                                  */
 
-  p_ci2 = p_i; // compiled case but it has run-time error  
+  p_ci = p_i; // compiled case but it has run-time error in Debug Mode(VS2010) and ok in Release 
   //  warning: uninitialized local variable 'p_i' used  
   // so we need to move it to below;
-  printf("*p_ci2: %d \n", *p_ci2); // print: currently uninitialized int 
+  printf("*p_ci2: %d(0x%x) \n", *p_ci2, *p_ci2); // print: currently uninitialized int 
 
   
-  printf("ci5: %d \n", ci5);    // print: ci5
+  printf("ci5: %d(0x%x) \n", ci5, ci5);    // print: ci5
  // printf("i: %d \n", i);      // print: i 
 }
